90e042aeb6702d55b3d4ea4f68870084d5a43283
Julia Mullen <julia.l.mullen@gmail.com>
2021-03-08 16:15:16 -0500

    incrementing id

--- parser.py
+++ parser.py
@@ -9,6 +9,6 @@ def load_annotations(data_folder):
     json_path = os.path.join(data_folder, "shapefiles.json.gz")
     with gzip.open(json_path) as f:
         data = json.loads(f.read().decode('utf-8'))
-    for datum in data:
-        datum['_id'] = datum['id']
+    for count, datum in enumerate(data):
+        datum['_id'] = count
         yield datum
