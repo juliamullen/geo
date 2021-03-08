5846e904f2296e32326dc5735974bfb572119194
Julia Mullen <julia.l.mullen@gmail.com>
2021-03-08 15:40:02 -0500

    check once a day, write id to _id

--- parser.py
+++ parser.py
@@ -8,4 +8,7 @@ logger = config.logger
 def load_annotations(data_folder):
     json_path = os.path.join(data_folder, "shapefiles.json.gz")
     with gzip.open(json_path) as f:
-        yield from json.loads(f.read().decode('utf-8'))
+        data = json.loads(f.read().decode('utf-8'))
+    for datum in data:
+        datum['_id'] = datum['id']
+        yield datum
