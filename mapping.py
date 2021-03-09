18feb74bd0d8f1a54461146a159748d0576421ba
juliamullen <julia.l.mullen@gmail.com>
2021-03-09 17:18:43 -0500

    Update mapping.py

--- mapping.py
+++ mapping.py
@@ -1,41 +1,38 @@
 def get_customized_mapping(cls):
     return {
-	"NAME_0": {
-	    "type": "keyword"
-	},
-	"NAME_1": {
-	    "type": "keyword"
-	},
-	"NAME_2": {
-	    "type": "keyword"
-	},
-	"admin_level": {
-	    "type": "integer"
-	},
-	"GID_0": {
-	    "type": "keyword"
-	},
-	"HASC_1": {
-	    "type": "keyword"
-	},
-	"HASC_2": {
-	    "type": "keyword"
-	},
-	"id": {
-	    "type": "integer"
-	},
-	"type": {
-	    "type": "keyword"
-	},
-	"geometry": {
-	    "properties": {
-		"type": {
-		    "normalizer": "keyword_lowercase_normalizer",
-		    "type": "keyword"
-		},
-		"coordinates": {
-		    "type": "float"
-		},
+	    "country": {
+		"type": "keyword"
+	    },
+	    "country_id": {
+		"type": "keyword"
+	    },
+	    "division": {
+		"type": "keyword"
+	    },
+	    "division_id": {
+		"type": "integer"
+	    },
+	    "location": {
+		"type": "keyword"
+	    },
+	    "location_id": {
+		"type": "keyword"
+	    },
+	    "id": {
+		"type": "keyword"
+	    },
+	    "type": {
+		"type": "keyword"
+	    },
+	    "geometry": {
+		"properties": {
+		    "type": {
+			"normalizer": "keyword_lowercase_normalizer",
+			"type": "keyword"
+		    },
+		    "coordinates": {
+			"type": "float"
+		    },
+		}
 	    }
 	}
-    }
