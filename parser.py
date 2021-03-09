35e91fdc85126314d8ab8c62d8b1106c16864faf
juliamullen <julia.l.mullen@gmail.com>
2021-03-09 17:43:20 -0500

    Update parser.py

--- parser.py
+++ parser.py
@@ -8,7 +8,4 @@ logger = config.logger
 def load_annotations(data_folder):
     json_path = os.path.join(data_folder, "shapefiles.json.gz")
     with gzip.open(json_path) as f:
-        data = json.loads(f.read().decode('utf-8'))
-    for count, datum in enumerate(data):
-        datum['_id'] = count
-        yield datum
+        yield from json.loads(f.read().decode('utf-8'))
